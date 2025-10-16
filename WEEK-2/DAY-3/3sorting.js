286270d7c50cbda78821c07f0a7d027a0e14488f
Rajesh Kumar Pradhan <rajeshpradhan9099@gmail.com>
2025-10-16 14:54:05 +0530

    new

---
+++ WEEK-2/DAY-3/3sorting.js
@@ -0,0 +1,10 @@
+let arr = [5, 2, 9, 1, 5];
+
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; 
+        }
+    }
+}
+console.log("Bubble Sort:", arr);
